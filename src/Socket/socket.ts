f56980d82c3c7e6d86d8abb74d31d8317040177f
Lucas Caresia <lucascarezia@gmail.com>
2025-07-28 10:18:58 -0300

    Added actions

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -3,6 +3,7 @@ import { randomBytes } from 'crypto'
 import { URL } from 'url'
 import { promisify } from 'util'
 import { proto } from '../../WAProto'
+import { cleanupQueues } from '../../WASignalGroup/queue_job'
 import {
 	DEF_CALLBACK_PREFIX,
 	DEF_TAG_PREFIX,
